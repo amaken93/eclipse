501fb96019578eba8a02903d453a10826f7335b8
amagai <amagai.k@eaz.co.jp>
2025-04-11 14:26:20 +0900

    課題02完了

--- Schoo-web/src/main/java/CalcServlet.java
+++ Schoo-web/src/main/java/CalcServlet.java
@@ -38,11 +38,50 @@ public class CalcServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		int num1 = Integer.parseInt(request.getParameter("num1"));
-		int num2 = Integer.parseInt(request.getParameter("num2"));
+		String operator = request.getParameter("operator");
+		String snum1 = request.getParameter("num1");
+		String snum2 = request.getParameter("num2");
+		String msg = null;
+		int calc = 0;
 
-		int calc = num1 + num2;
+		try {
+			int num1 = Integer.parseInt(snum1);
+			int num2 = Integer.parseInt(snum2);
+			switch (operator) {
+			case "plus":
+				calc = num1 + num2;
+				break;
 
+			case "minus":
+				calc = num1 - num2;
+				break;
+
+			case "multiply":
+				calc = num1 * num2;
+				break;
+
+			case "divide":
+				if (num2 == 0) {
+					msg = "0で割ることはできません";
+					break;
+				}
+				calc = num1 / num2;
+				break;
+
+			default:
+				msg = "error";
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			if (snum1.isEmpty() || snum2.isEmpty()) {
+				msg = "数値を入力してください";
+			} else {
+				msg = "数値を入力してください";
+			}
+
+		}
+		request.setAttribute("msg", msg);
 		request.setAttribute("calc", calc);
 		RequestDispatcher rd = request.getRequestDispatcher("output2.jsp");
 		rd.forward(request, response);
--- Schoo-web/src/main/webapp/output2.jsp
+++ Schoo-web/src/main/webapp/output2.jsp
@@ -1,12 +1,29 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<link rel="stylesheet" href="calc.css">
 </head>
 <body>
-<p>計算結果は<%=request.getAttribute("calc") %>です。</p>
+	<h1>計算結果</h1>
+	<%
+	if (request.getAttribute("msg") == null) {
+	%>
+	<p>
+		計算結果は<%=request.getAttribute("calc")%>です
+	</p>
+	<%
+	} else {
+	%>
+
+	<p class="error"><%=request.getAttribute("msg")%></p>
+	<%
+	}
+	%>
+	<br>
+	<a href="input2.html">戻る</a>
 </body>
 </html>
